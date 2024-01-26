5e3c206c87d34be5154c1acfe26e0521ce73eee9
rikeshg <garlapati.rc@gmail.com>
2024-01-25 23:43:57 -0800

    one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rikeshcars';
+  box = 'Rikky';
+  imagePath = '../assets/Huskiesatrest.jpg';
+  name = "";
 }
